636169d2ef982f180022fbd9b9681c938eacee12
Alexandre BOREL <alexandreborel1994@gmail.com>
2019-06-18 16:55:12 +0200

    Missing properties

--- ios/RNBraintreeLite.h
+++ ios/RNBraintreeLite.h
@@ -12,4 +12,23 @@
 
 @interface RNBraintreeLite : NSObject <RCTBridgeModule>
 
+@property (nonatomic, strong) UIViewController *_Nonnull reactRoot;
+
+// Retain your `BTDataCollector` instance for your entire application lifecycle.
+@property (nonatomic, strong) BTDataCollector *_Nonnull dataCollector;
+
+@property (nonatomic, strong) BTAPIClient *_Nonnull braintreeClient;
+
+@property (nonatomic, strong) PKPaymentRequest *_Nonnull paymentRequest;
+
+@property (nonatomic, strong) PKPaymentAuthorizationViewController *_Nonnull viewController;
+
+@property (nonatomic, strong) NSString * _Nonnull deviceDataCollector;
+
+@property (nonatomic) RCTPromiseResolveBlock _Nonnull resolve;
+
++ (void)resolvePayment:(BTDropInResult* _Nullable)result deviceData:(NSString * _Nonnull)deviceDataCollector resolver:(RCTPromiseResolveBlock _Nonnull)resolve;
+
+
+
 @end
